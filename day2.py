92c1a7ce9674d911fd147c0a22446124d472e52e
yumiko-s <saitoh.fw@gmail.com>
2025-12-26 10:33:10 +0900

    Day2: variables, if, for

---
+++ day2.py
@@ -0,0 +1,13 @@
+name = "Yumiko"
+age = 57
+
+print("My name is ", name)
+print("I am ", age, "years old")
+
+if age >= 20:
+    print("You are an adult")
+else:
+    print("You are a child")
+
+for i in range(5):
+    print("Count : ", i)
