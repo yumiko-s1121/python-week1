b5437ec6cd376893691846cc2ce6cfd1ed55f0bd
yumiko-s <saitoh.fw@gmail.com>
2025-12-26 10:45:20 +0900

    Day2: list and loop

---
+++ day2_list.py
@@ -0,0 +1,16 @@
+names = {"Yumiko", "John", "Amy", "Ken"}
+
+print("Member list:")
+
+for name in names:
+    print(name)
+
+score = {80, 95, 70, 60}
+
+total = 0
+
+for s in score :
+    total += s
+
+print("Total : ", total)
+print("Average : ", total / len(score))
